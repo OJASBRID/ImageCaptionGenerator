8ab0f2ae4c6e56326a38a1b0314a76d3c55cbcfd
Ojas Brid <ojasbc21@iitk.ac.in>
2024-08-31 23:33:55 +0530

    Data Generator

--- main_project.py
+++ main_project.py
@@ -189,4 +189,45 @@ def create_tokenizer(descriptions):
 tokenizer = create_tokenizer(train_descriptions)
 dump(tokenizer, open('tokenizer.p', 'wb'))
 vocab_size = len(tokenizer.word_index) + 1
-vocab_size 
+
+def max_length(descriptions):
+    desc_list = dict_to_list(descriptions)
+    return max(len(d.split()) for d in desc_list)
+    
+max_length = max_length(descriptions)
+print(max_length)
+
+def create_sequences(tokenizer, max_length, desc_list, feature):
+    x_1, x_2, y = list(), list(), list()
+    # move through each description for the image
+    for desc in desc_list:
+        # encode the sequence
+        seq = tokenizer.texts_to_sequences([desc])[0]
+        print(len(seq))
+        # divide one sequence into various X,y pairs
+        for i in range(1, len(seq)):
+            # divide into input and output pair
+            in_seq, out_seq = seq[:i], seq[i]
+            # pad input sequence
+            in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
+            # encode output sequence
+            out_seq = to_categorical([out_seq], num_classes=vocab_size)[0]
+            # store
+            x_1.append(feature)
+            x_2.append(in_seq)
+            y.append(out_seq)
+            
+    return np.array(x_1), np.array(x_2), np.array(y)
+
+def data_generator(descriptions, features, tokenizer, max_length):
+    while 1:
+        for key, description_list in descriptions.items():
+            #retrieve photo features
+            feature = features[key + ".jpg"][0]
+            inp_image, inp_sequence, op_word = create_sequences(tokenizer, max_length, description_list, feature)
+            yield [[inp_image, inp_sequence], op_word]
+    
+
+#To check the shape of the input and output for your model
+[a, b], c = next(data_generator(train_descriptions, features, tokenizer, max_length))
+a.shape, b.shape, c.shape
